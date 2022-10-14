1a86346f0a7d0ce615c5d109a8a2dc59d6172066
Ecnac <101355118+Ecnac@users.noreply.github.com>
2022-10-14 17:43:50 +0200

    Users signup, login, check if connected

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 // check if the user is authenticated
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')('_')[1];
+        const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.TOKEN);
         const userId = decodedToken.userId;
         req.auth = {
